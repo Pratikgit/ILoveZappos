c1cfca178659d54cbca785eebce983b03ccf9da7
Pratikbit <pratik.bhangale1992@gmail.com>
2017-01-27 01:32:29 -0500

    - Adding basic models for Product and ProductResponse

---
+++ app/src/main/java/umbc/edu/ilovezappos/models/ProductsResponse.java
@@ -0,0 +1,78 @@
+package umbc.edu.ilovezappos.models;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by Pratik on 25-01-2017.
+ */
+
+public class ProductsResponse {
+
+    private String originalTerm;
+    private String currentResultCount;
+    private String totalResultCount;
+    private String term;
+    private List<Product> results = null;
+    private String statusCode;
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    public String getOriginalTerm() {
+        return originalTerm;
+    }
+
+    public void setOriginalTerm(String originalTerm) {
+        this.originalTerm = originalTerm;
+    }
+
+    public String getCurrentResultCount() {
+        return currentResultCount;
+    }
+
+    public void setCurrentResultCount(String currentResultCount) {
+        this.currentResultCount = currentResultCount;
+    }
+
+    public String getTotalResultCount() {
+        return totalResultCount;
+    }
+
+    public void setTotalResultCount(String totalResultCount) {
+        this.totalResultCount = totalResultCount;
+    }
+
+    public String getTerm() {
+        return term;
+    }
+
+    public void setTerm(String term) {
+        this.term = term;
+    }
+
+    public List<Product> getResults() {
+        return results;
+    }
+
+    public void setResults(List<Product> results) {
+        this.results = results;
+    }
+
+    public String getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(String statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
+
